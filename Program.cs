1192ee8da52948a9f61191c9537bb3f561d7023a
DEV <dev@info.local>
2020-11-13 10:31:59 -0300

    exercício 1 adicionado

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Exercício_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite sua idade");
+            int idade = int.Parse(Console.ReadLine());
+
+            int meses = idade*12;
+            Console.WriteLine(meses +" meses");
+
+            int dias = idade*365;
+            Console.WriteLine(dias +" dias");
+
+            int horas = dias*24;
+            Console.WriteLine(horas +" horas");
+
+            int minutos = horas*60;
+            Console.WriteLine(minutos +" minutos");
+
+        }//FIM DO MAIN
+    }
+}
